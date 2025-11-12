32f1aff637b1021fec6c5ad091b44507606104b2
ivandax <ivandax_89@hotmail.com>
2025-11-12 16:30:07 +0100

    Add edit profile name

--- src/presentation/views/MyProfile/MyProfile.tsx
+++ src/presentation/views/MyProfile/MyProfile.tsx
@@ -1,17 +1,58 @@
+import { getUser, updateUser } from "@src/persistence/user";
 import * as S from "./MyProfile.Styles";
 import { MainButton } from "@src/presentation/components/Buttons/MainButton";
 import { useAuthStore } from "@src/presentation/store/authStore";
+import { notifyError, notifySuccess } from "@src/presentation/utils";
+import { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { AppUser } from "@src/domain/AppUser";
 
 function MyProfile() {
   const navigate = useNavigate();
   const { userTask, logout } = useAuthStore();
   const user = userTask.status === "successful" ? userTask.data : null;
+  const [userProfile, setUserProfile] = useState<null | AppUser>(null);
+
+  const handleGetProfile = useCallback(
+    async (id: string) => {
+      const result = await getUser(id);
+      if (result.error) {
+        notifyError("Error getting user");
+        return;
+      }
+      if (result.data === null) {
+        notifyError("Error getting user = null");
+        return;
+      }
+      setUserProfile(result.data);
+    },
+    [setUserProfile] // Dependencies
+  );
+
+  useEffect(() => {
+    if (user) {
+      handleGetProfile(user.id);
+    }
+  }, [user, handleGetProfile]);
 
   const handleLogout = async () => {
     await logout();
     navigate("/login");
   };
+
+  const handleNameBlur = useCallback(async () => {
+    if (userProfile) {
+      const result = await updateUser(
+        { name: userProfile.name },
+        userProfile.id
+      );
+      if (result.error) {
+        notifyError("Error updating user name");
+        return;
+      }
+      notifySuccess("User name updated successfully");
+    }
+  }, [userProfile]);
 
   return (
     <S.Wrapper>
@@ -25,6 +66,17 @@ function MyProfile() {
           <S.InfoLine>
             <strong>Email:</strong> {user.email}
           </S.InfoLine>
+          {userProfile && (
+            <input
+              type="text"
+              value={userProfile.name}
+              onChange={(e) =>
+                setUserProfile({ ...userProfile, name: e.target.value })
+              }
+              placeholder="Display name"
+              onBlur={handleNameBlur}
+            />
+          )}
         </S.ProfileInfo>
       ) : (
         <p>Loading user data...</p>
